4ee01112bd63779893c0b2fa46c665a628fb62de
sab6794 <86272161+sab6794@users.noreply.github.com>
2021-07-02 13:38:49 -0400

    Update about.vue
    Removed the stylesheet

--- ui/samislost/pages/about.vue
+++ ui/samislost/pages/about.vue
@@ -42,45 +42,4 @@ export default {};
 console.log(Vue.version);
 </script>
 
-<style>
-.container {
-  margin: 0 auto;
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
-
-.credits {
-  padding-top: 45px;
-  margin: 0 auto;
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  text-align: center;
-}
-
-.title {
-  font-family: "Quicksand", "Source Sans Pro", -apple-system, BlinkMacSystemFont,
-    "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
-}
-</style>
 
