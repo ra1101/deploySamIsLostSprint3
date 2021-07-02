68c2cc29d5e9764e1ba4280bf65f9b7343f4a749
sab6794 <86272161+sab6794@users.noreply.github.com>
2021-07-02 13:39:13 -0400

    Update index.vue
    Added the information about the details of a sighting

--- ui/samislost/pages/list/_id/index.vue
+++ ui/samislost/pages/list/_id/index.vue
@@ -1,9 +1,37 @@
 <template>
   <div>
-    <nuxt-link to="/list">Back to sightings</nuxt-link>
-    <h2>{{ sighting }}</h2>
+    <p class="link"><nuxt-link to="/">Back to sightings</nuxt-link></p>
+    <table class="Table">
+      <tr>
+        <th colspan=2 >{{ sighting.title }}</th>
+      </tr>
+      <tr>
+        <td class="left">Type</td>
+        <td class="right">{{ sighting.type }}</td>
+      </tr>      
+      <tr>
+        <td class="left">Entry Date/Time</td>
+        <td class="right">{{sighting.entryDate}} {{ sighting.entryTime }}</td>
+      </tr>
+      <tr>
+        <td class="left">Sighted Date/Time</td>
+        <td class="right">{{sighting.sightingDate}} {{ sighting.sightingTime }} {{sighting.timeZone}}</td>
+      </tr>
+      <tr>
+        <td class="left">Sighted Location</td>
+        <td class="right">{{ sighting.location }}</td>
+      </tr>      
+      <tr>
+        <td class="left">Current Location</td>
+        <td class="right"></td>
+      </tr>
+      <tr>
+        <td class="left">Description</td>
+        <td class="right">{{ sighting.description }}</td>
+      </tr>
+    </table>
+    <br>
     <hr />
-    <small>Sighting ID: {{ $route.params.id }}</small>
   </div>
 </template>
 
@@ -21,18 +49,28 @@ export default {
         Accept: "application/json",
       },
     };
-
+    const routeId = this.$route.params.id;
     try {
       const res = await axios.get(
-        "http://localhost:3004/s/${this.$route.params.id}",
+        'http://localhost:3004/sightings/' + routeId,
         config
       );
-
-      this.sighting = res.data.sighting;
+      this.sighting = res.data;
     } catch (err) {
       console.log(err);
     }
   },
+  head() {
+    return {
+      title: this.sighting.title,
+      meta: [
+        {
+          hid: "description",
+          name: "description",
+          content: "Best way to get animals back home"
+        }
+      ]
+    };
+  }
 };
 </script>
-
