ff973b7a589f678fedf6dc0408fb3d8fe019605a
Jeff Chen <adejeffchen@gmail.com>
2021-07-07 23:29:50 -0700

    for https://team-turtle.atlassian.net/browse/SIL-4, added unit tests for create entry

---
+++ ui/samislost/tests/unit/CreateEntry.spec.js
@@ -0,0 +1,47 @@
+import axios from "axios";
+
+async function createEntry(form) {
+  const response = await axios.post("http://localhost:3004/sightings", form);
+  return response.status;
+};
+
+// -------------------------------------------------------
+// testing create entry 
+// -------------------------------------------------------
+describe('create entry feature', () => {
+  it('makes sure unit test runs', () => {
+    expect(true).toBe(true);
+  })
+})
+
+describe('create entry feature', () => {
+  it('create successfully', async () => {
+    var form = {};
+    form.type == "dog";
+    form.title == "abc";
+    form.sightingDate == "x";
+    form.sightingTime == "x";
+    form.sightedLocation == "x";
+    form.currentLocation == "x";
+    form.description == "x";
+    return createEntry(form).then(data => {
+      expect(data).toBe(201);
+    });
+  });
+})
+
+describe('create entry feature', () => {
+  it('create with blank', async () => {
+    var form = {};
+    form.type == "";
+    form.title == "";
+    form.sightingDate == "";
+    form.sightingTime == "";
+    form.sightedLocation == "";
+    form.currentLocation == "";
+    form.description == "";
+    return createEntry(form).then(data => {
+      expect(data).toBe(201);
+    });
+  });
+})
